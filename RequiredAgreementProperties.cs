17b645489bc26ff451affe8026e2a9c1f0c8b703
Matthew Sandstrom <MatthewS@kentico.com>
2023-02-01 17:05:43 -0500

    Add project files.

---
+++ RequiredAgreementProperties.cs
@@ -0,0 +1,55 @@
+﻿using CMS.DataEngine;
+using Kentico.Forms.Web.Mvc;
+
+namespace CustomFormComponent.RequiredAgreement
+{
+
+    public class RequiredAgreementProperties : FormComponentProperties<bool>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckBoxProperties"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The constructor initializes the base class to data type <see cref="FieldDataType.Boolean"/>.
+        /// </remarks>
+        public RequiredAgreementProperties() : base(FieldDataType.Boolean)
+        {
+        }
+
+
+        /// <summary>
+        /// Gets or sets the default value of the form component and underlying field.
+        /// </summary>
+        [DefaultValueEditingComponent(CheckBoxComponent.IDENTIFIER)]
+        public override bool DefaultValue
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Represents the input value in the resulting HTML.
+        /// </summary>
+        [EditingComponent(TextInputComponent.IDENTIFIER, Label = "{$kentico.formbuilder.component.checkbox.properties.text$}")]
+        public string Text
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Represents whether the form component is required.
+        /// </summary>
+        /// <remarks>
+        /// Assigning no editing control means the required checkbox will not be visible.
+        /// This control will always behave as if it is required regardless of this property, but returning true here will make the property consistent for anyone inspecting the component properties while debugging.
+        /// </remarks>
+        public override bool Required 
+        {
+            get => true;
+            set => base.Required = value; 
+        }
+    }
+}
